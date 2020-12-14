24d4242297a62dd343e4d39e567678ffae7dda9e
Joshua Crowley Nealon <S00198699@mail.itsligo.ie>
2020-12-14 10:48:14 +0000

    part 3 fanito

--- CA2_2020/Employee.cs
+++ CA2_2020/Employee.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace CA2_2020
 {
-    class Employee
+    abstract class Employee
     {
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
     }
 }
