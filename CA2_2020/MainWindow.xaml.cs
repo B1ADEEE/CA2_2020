2d05231d1d0141d154f189f0daf80207f96408a4
Joshua Crowley Nealon <S00198699@mail.itsligo.ie>
2020-12-14 18:14:49 +0000

    stuff sorta question 9

--- CA2_2020/MainWindow.xaml.cs
+++ CA2_2020/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,54 @@ namespace CA2_2020
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> FirstName = new List<string>();
+        List<string> surName = new List<string>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ListBoxEmployeeBOX_Loaded(object sender, RoutedEventArgs e)
+        {
+            //string[] names = { "Jane Jones PT", "Joe Murphy FT", "John Smith PT", "Jess Walsh FT" };
+            FirstName.Add("Jane");
+            FirstName.Add("Joe");
+            FirstName.Add("John");
+            FirstName.Add("Jess");
+            surName.Add("Jones");
+            surName.Add("Murphy");
+            surName.Add("Smith");
+            surName.Add("Walsh");
+            ListBoxEmployeeBOX.ItemsSource = FirstName;
+            ListBoxEmployeeBOX.ItemsSource = surName;
+        }
+
+        private void FirstNameTBX_GotFocus(object sender, RoutedEventArgs e)
+        {
+            FirstNameTBX.Clear();
+        }
+
+        private void AddBTN_Click(object sender, RoutedEventArgs e)
+        {
+            string newFirst = FirstNameTBX.Text;
+            string newSur = SurnameTBX.Text;
+            FirstName.Add(newFirst);
+            surName.Add(newSur);
+        }
+
+        private void ClearBTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UpdateBTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteBTN_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
