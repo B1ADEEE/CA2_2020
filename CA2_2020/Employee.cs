c8fcb9bdda4eaf1c477b8d33d1c5ebaa827b562b
Joshua Crowley Nealon <S00198699@mail.itsligo.ie>
2020-12-14 11:14:16 +0000

    up to part 5 done i think

--- CA2_2020/Employee.cs
+++ CA2_2020/Employee.cs
@@ -6,9 +6,27 @@ using System.Threading.Tasks;
 
 namespace CA2_2020
 {
-    abstract class Employee
+    public abstract class Employee
     {
+        //PROPERTIES
         public string FirstName { get; set; }
         public string SurName { get; set; }
+
+        //METHODS
+        public abstract decimal CalculateMonthlyPay();
+    }
+    public class FullTimeEmplyee : Employee
+    {
+        public override decimal CalculateMonthlyPay()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class PartTimeEmplyee : Employee
+    {
+        public override decimal CalculateMonthlyPay()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
